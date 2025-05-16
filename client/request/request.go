f330f33ab16a7bd9b5c466ea88e120ceefd0208b
Keshav Acharya <keshav.ach@outlook.com>
2025-05-16 15:04:35 -0700

    cleaned up

--- client/request/request.go
+++ client/request/request.go
@@ -7,7 +7,6 @@ import (
 	"encoding/json"
 	"io"
 	"os"
-	"strconv"
 
 	// "crypto/tls"
 	"crypto/x509"
@@ -167,119 +166,6 @@ func (r *RequestTLS) Call(hsOnly bool) (RequestData, error) {
 	msg, _ := io.ReadAll(resp.Body)
 	log.Info().Msg("response data:")
 	log.Info().Msg(string(msg))
-
-	// catch time
-	elapsed = time.Since(start)
-	log.Debug().Str("time", elapsed.String()).Msg("client request-response roundtrip took.")
-
-	// access to recorded session data
-	return RequestData{
-		secrets:   conn.GetSecretMap(),
-		recordMap: conn.GetRecordMap(),
-	}, nil
-}
-
-func (r *RequestTLS) Call2(hsOnly bool) (RequestData, error) {
-
-	// tls configs
-	config := &tls.Config{
-		InsecureSkipVerify:       false,
-		CurvePreferences:         []tls.CurveID{tls.CurveP256},
-		PreferServerCipherSuites: false,
-		// MinVersion:               tls.VersionTLS13,
-		// MaxVersion:               tls.VersionTLS13,
-		CipherSuites: []uint16{
-			tls.TLS_ECDHE_ECDSA_WITH_AES_128_CBC_SHA,
-			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-		},
-		// ServerName: r.ServerDomain,
-	}
-
-	// local server testing settings
-	if r.ServerDomain == "localhost" {
-		PathCaCrt := "../certs/certificates/ca.crt"
-
-		// set up cert verification
-		caCert, _ := os.ReadFile(PathCaCrt)
-		caCertPool, _ := x509.SystemCertPool()
-		caCertPool.AppendCertsFromPEM(caCert)
-		config.RootCAs = caCertPool
-
-		r.ServerDomain += ":8081"
-	}
-
-	// measure start time
-	start := time.Now()
-
-	// tls connection
-	conn, err := tls.Dial("tcp", "localhost:8081", config)
-	if err != nil {
-		log.Error().Err(err).Msg("tls.Dial()")
-		return RequestData{}, err
-	}
-	defer conn.Close()
-
-	// tls handshake time
-	elapsed := time.Since(start)
-	log.Debug().Str("time", elapsed.String()).Msg("client tls handshake took.")
-	// state := conn.ConnectionState()
-
-	// return here if handshakeOnly flag set
-	if hsOnly {
-		return RequestData{}, nil
-	}
-
-	// server settings
-	serverURL := "https://" + r.ServerDomain + r.ServerPath
-	if r.UrlPrivateParts != "" {
-		serverURL += r.UrlPrivateParts
-	}
-
-	// measure request-response roundtrip
-	start = time.Now()
-
-	// build request
-	request, _ := http.NewRequest(http.MethodGet, serverURL, nil)
-	request.Close = false
-
-	// request headers
-	request.Header.Set("Content-Type", "application/json")
-	if r.AccessToken != "" {
-		request.Header.Set("Authorization", "Bearer "+r.AccessToken)
-	}
-
-	// initialize connection buffers
-	bufr := bufio.NewReader(conn)
-	bufw := bufio.NewWriter(conn)
-
-	// write request to connection buffer
-	err = request.Write(bufw)
-	if err != nil {
-		log.Error().Err(err).Msg("request.Write(bufw)")
-		return RequestData{}, err
-	}
-
-	// writes buffer data into connection io.Writer
-	err = bufw.Flush()
-	if err != nil {
-		log.Error().Err(err).Msg("bufw.Flush()")
-		return RequestData{}, err
-	}
-
-	// read response
-	resp, err := http.ReadResponse(bufr, request)
-	if err != nil {
-		log.Error().Err(err).Msg("http.ReadResponse(bufr, request)")
-		return RequestData{}, err
-	}
-	defer resp.Body.Close()
-
-	// reads response body
-	msg, _ := io.ReadAll(resp.Body)
-	log.Info().Msg("response data:")
-	log.Info().Msg(string(msg))
-	sizeStr := strconv.Itoa(len(msg))
-	log.Info().Msg(sizeStr)
 
 	// catch time
 	elapsed = time.Since(start)
